85d444fd7a7c87e2be9324bf37542709e61806fd
Muckbuck <rosenborg.eric@gmail.com>
2018-05-03 11:58:25 +0200

    Final version.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 $(document).ready(function() {
-    $(function() {
-        $('[data-toggle="tooltip"]').tooltip()
-    })
+
+    var localstorage = window.localStorage;
+    var currentSection = localstorage.getItem('currentSection');
 
     var radio_1 = $('#radio-1');
     var radio_2 = $('#radio-2');
     var radio_3 = $('#radio-3');
+    var radio_4 = $('#radio-4');
+
+    radio_1.prop('checked', false);
+    $('#radio-' + currentSection).prop('checked', true);
 
     window.addEventListener('scroll', function(e) {
         var element = $(":focus")
@@ -15,17 +19,18 @@ $(document).ready(function() {
         radio_1.prop('checked', true);
         radio_2.prop('checked', false);
         radio_3.prop('checked', false);
+        radio_4.prop('checked', false);
+        localStorage.setItem('currentSection', '1');
         document.querySelector('#section-1').scrollIntoView({
             behavior: 'smooth'
         });
-    });
-    radio_1.hover(function() {
-
     });
     radio_2.click(function() {
         radio_1.prop('checked', false);
         radio_2.prop('checked', true);
         radio_3.prop('checked', false);
+        radio_4.prop('checked', false);
+        localStorage.setItem('currentSection', '2');
         document.querySelector('#section-2').scrollIntoView({
             behavior: 'smooth'
         });
@@ -34,7 +39,19 @@ $(document).ready(function() {
         radio_1.prop('checked', false);
         radio_2.prop('checked', false);
         radio_3.prop('checked', true);
+        radio_4.prop('checked', false);
+        localStorage.setItem('currentSection', '3');
         document.querySelector('#section-3').scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+    radio_4.click(function() {
+        radio_1.prop('checked', false);
+        radio_2.prop('checked', false);
+        radio_3.prop('checked', false);
+        radio_4.prop('checked', true);
+        localStorage.setItem('currentSection', '4');
+        document.querySelector('#section-4').scrollIntoView({
             behavior: 'smooth'
         });
     });
