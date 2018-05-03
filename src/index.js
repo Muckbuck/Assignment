6742a69f3720435ac8a8e5afebf6428bd2b27207
Muckbuck <rosenborg.eric@gmail.com>
2018-05-03 03:42:01 +0200

    Added a responsive version for phones and Ipad. Might need to add some more breakpoints. Also repositioned the menu items and also the section image and jumobtron.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,11 @@
 $(document).ready(function() {
-
-    $.fn.scrollView = function() {
-        return this.each(function() {
-            $('html, body').animate({
-                scrollTop: $(this).offset().top
-            }, 1000);
-        });
-    }
+    $(function() {
+        $('[data-toggle="tooltip"]').tooltip()
+    })
 
     var radio_1 = $('#radio-1');
     var radio_2 = $('#radio-2');
     var radio_3 = $('#radio-3');
-
-    console.log($('#section-1').position().top);
-    console.log($('#section-1').position().bott);
 
     window.addEventListener('scroll', function(e) {
         var element = $(":focus")
@@ -23,16 +15,17 @@ $(document).ready(function() {
         radio_1.prop('checked', true);
         radio_2.prop('checked', false);
         radio_3.prop('checked', false);
-        var y = $('#section-1').position().top;
         document.querySelector('#section-1').scrollIntoView({
             behavior: 'smooth'
         });
+    });
+    radio_1.hover(function() {
+
     });
     radio_2.click(function() {
         radio_1.prop('checked', false);
         radio_2.prop('checked', true);
         radio_3.prop('checked', false);
-        var y = $('#section-2').position().top;
         document.querySelector('#section-2').scrollIntoView({
             behavior: 'smooth'
         });
@@ -41,7 +34,6 @@ $(document).ready(function() {
         radio_1.prop('checked', false);
         radio_2.prop('checked', false);
         radio_3.prop('checked', true);
-        var y = $('#section-3').position().top;
         document.querySelector('#section-3').scrollIntoView({
             behavior: 'smooth'
         });
